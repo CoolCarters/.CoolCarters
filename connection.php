6813131991f2119ba32f37a23b6c055251c93e7a
Nirjal22 <bistanirjal978@gmail.com>
2025-05-14 19:58:25 +0545

    Resolved merge conflict in login.php

--- connection.php
+++ connection.php
@@ -1,12 +1,8 @@
-<?php 
-
-$conn = oci_connect('SYSTEM', 'Bimala321', '//localhost/xe'); if (!$conn) {
-    $m = oci_error($conn);
-    echo $m['message'], "\n";
-    exit; } else {
- } 
- 
- $conn = oci_connect('SYSTEM','Bimala321', '//localhost/xe') or die("Unable to connect to database");
-     oci_close($conn);
-
-?>
+<?php $conn = oci_connect('CoolCarters', 'coolcarters', '//localhost/xe'); if (!$conn) {
+   $m = oci_error();
+   echo $m['message'], "\n";
+   exit; } else {
+   print "Connected to Oracle!"; } 
+    oci_close($conn); 
+    ?>
+    
